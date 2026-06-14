81040c00a1fd67026be55ba691dab1e28ef5e720
ksv <kalaasrivarshini@gmail.com>
2026-06-14 20:18:18 +0530

    chore: remove remaining legacy prototypes, unused mocks, and dead components

--- apps/web/src/data/mock/missions.ts
+++
@@ -1,8 +0,0 @@
-/** @deprecated Import from mission-catalog.ts */
-export {
-  allMissions as mockMissions,
-  getMissionById,
-  getMissionForSdg,
-} from '@/data/mock/mission-catalog'
-
-export type { Mission } from '@/types/mission'
